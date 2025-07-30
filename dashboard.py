3413bf070acec3586b95ae4207ad3158afd495ce
12625 <1262573625@qq.com>
2025-07-31 00:06:28 +0800

    添加日志打印,获取币种名称,查询币种k线数据功能

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,44 @@
+import okx.PublicData
+import okx.MarketData
+
 import tkinter as tk
 from tkinter import ttk
 import utils
+
+logger = utils.get_logger()
+
+FLAG = 1    # 0: real trade, 1: simulate trade
+
+def get_all_coin_name(coin_type='SWAP'):
+    coin_names = []
+    try:
+        publicdata_api = okx.PublicData.PublicAPI(flag=FLAG)
+        rst = publicdata_api.get_instruments(instType=coin_type)
+        if not rst['code']:
+            logger.error('get coin name error. return code is %s, error occur: %s', rst['code'], rst['msg'])
+            raise Exception('get coin name error')
+        coin_names = [i for i in {j['instFamily'] for j in rst['data']}]
+        coin_names.sort()
+    except Exception as e:
+        logger.exception('get all coin name via public data api error: %s', e)
+    finally:
+        return coin_names
+
+def get_kline_data(timespan=15):
+    """get recently 15 days coin kline data"""
+    coin_names = get_all_coin_name()
+    if not coin_names:
+        logger.error('try to get all coin names but result is empty. can not continue get k line data')
+    marketdata_api = okx.MarketData.MarketAPI(flag=FLAG)
+    coin_kline_data = {}
+    try:
+        for coin_name in coin_names:
+            rst = marketdata_api.get_candlesticks(instId=coin_name, bar='1D', limit=timespan)
+            coin_kline_data[coin_name] = rst['data']
+    except Exception as e:
+        logger.exception('get coin kline data error: %s', e)
+    finally:
+        return coin_kline_data
 
 
 def data_collect_panel(notebook):
--- utils.py
+++ utils.py
@@ -1,3 +1,25 @@
+import os
+import logging
+
+LOG_PATH = os.path.join(os.path.realpath('.'), 'trade.log')
+
+def get_logger():
+    log_path_real = os.path.realpath(LOG_PATH)
+    if not os.path.exists(log_path_real):
+        with open(log_path_real, 'w'):
+            pass
+    log_format = logging.Formatter(
+        "%(asctime)s %(levelname)s [%(name)s] [%(filename)s] [%(funcName)s:%(lineno)d] [PID:%(process)d TID:%(thread)d] %(message)s")
+    logger = logging.getLogger('trade')
+    logger.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler(LOG_PATH)
+    file_handler.setFormatter(log_format)
+
+    logger.addHandler(file_handler)
+
+    return logger
+
 def activate_widget(*args):
     for i in args:
         i['state'] = 'normal'
